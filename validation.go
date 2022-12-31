f419a00ca1041cce51193dd79c990426a482e598
ejuju <juju.sel.mail@gmail.com>
2022-12-31 02:28:14 +0100

    add utilitiy function

--- validation.go
+++ validation.go
@@ -2,7 +2,9 @@ package validation
 
 import (
 	"errors"
+	"fmt"
 	"net/mail"
+	"unicode/utf8"
 )
 
 func Check(errmsg string, checks ...bool) error {
@@ -26,4 +28,28 @@ func CheckAll(errs ...error) error {
 func IsEmailAddr(in string) bool {
 	_, err := mail.ParseAddress(in)
 	return err == nil
+}
+
+func CheckEmailAddress(in string) error {
+	return CheckAll(
+		CheckStringLength("email", in, 1, 300),
+		Check(
+			fmt.Sprintf("invalid email address: %s", in),
+			IsEmailAddr(in),
+		),
+	)
+}
+
+func CheckStringLength(name, in string, min, max int) error {
+	return Check(
+		fmt.Sprintf(
+			"%s is not within length [%d;%d]: %d chars",
+			name,
+			min,
+			max,
+			utf8.RuneCountInString(in),
+		),
+		utf8.RuneCountInString(in) >= min,
+		utf8.RuneCountInString(in) <= max,
+	)
 }
